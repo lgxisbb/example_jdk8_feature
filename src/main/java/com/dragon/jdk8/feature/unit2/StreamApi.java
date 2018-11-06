a49ebb4f728894d6f9be89d8f5996376a82be6a9
lgxisbb@163.com <lgxisbb@163.com>
2018-11-06 18:29:29 +0800

    Stream Api example

--- src/main/java/com/dragon/jdk8/feature/unit2/StreamApi.java
+++ src/main/java/com/dragon/jdk8/feature/unit2/StreamApi.java
@@ -5,7 +5,8 @@ import java.util.stream.Stream;
 /**
  * @Author Lucky Dragon
  * @CreateTime 2018/11/1 23:02
- * @Description stream() 是Collections的一个方法, 可以把任何的集合类转换为Stream
+ * @Description \n
+ * stream() 是Collections的一个方法, 可以把任何的集合类转换为Stream
  * 如果是数组, 可以使用 Stream.of(T[]), 获得一个Stream
  */
 public class StreamApi {
